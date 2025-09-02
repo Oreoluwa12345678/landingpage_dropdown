15a9fc92bce5e480807291eb9c5496b295090045
Oreoluwa12345678 <olufemioreoluwa555@gmail.com>
2025-09-02 12:51:03 +0100

    made some few changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,16 +14,16 @@ const Hero = () => {
     return () => window.removeEventListener("resize", handleWidth);
   }, []);
   return (
-    <section className="flex md:flex-row flex-col-reverse items-center md:items-end justify-between w-full md:w-10/12 ml-auto mr-auto md:h-[30rem]">
+    <section className="flex md:flex-row flex-col-reverse items-center md:items-end justify-between w-full md:w-10/12 ml-auto mr-auto md:h-[30rem] mt-0 md:mt-[10rem]">
       <div className="text-center md:text-left">
-        <h3 className="text-2xl md:text-7xl font-bold text-gray-950 mb-5">
+        <h3 className="text-3xl md:text-7xl font-bold text-gray-950 mb-5 md:mb-12">
           Make <br className="hidden md:block" /> remote work
         </h3>
-        <p className="w-80 md:w-96 text-sm text-gray-500 font-medium mb-7 md:mb-10 ml-auto mr-auto md:ml-0 md:mr-0">
+        <p className="w-80 md:w-96 text-sm text-gray-500 font-medium mb-7 md:mb-8 ml-auto mr-auto md:ml-0 md:mr-0">
           Get your team in sync, no matter your location. Streamline processes,
           create team rituals, and watch productivity soar.
         </p>
-        <button className="bg-gray-950 text-gray-50 px-5 py-3 text-sm font-medium rounded-lg mb-7 md:mb-16 hover:cursor-pointer hover:bg-white hover:border-2 hover:border-gray-950 hover:text-black">
+        <button className="bg-gray-950 text-gray-50 px-5 py-3 text-sm font-medium rounded-lg mb-7 md:mb-24 hover:cursor-pointer hover:bg-white hover:border-2 hover:border-gray-950 hover:text-black">
           Learn more
         </button>
         <div className="flex gap-6 md:gap-4 justify-start items-center md:mb-0 mb-6">
@@ -37,7 +37,7 @@ const Hero = () => {
       <img
         src={width > 768 ? heroDesktop : heroMobile}
         alt=""
-        className="w-full md:w-[22rem] md:mb-0 mb-8"
+        className="w-full md:w-[27rem] md:mb-0 mb-8"
       />
     </section>
   );
